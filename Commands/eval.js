6b7251e9d53db816e73882add2ad254149166c5d
citizenofcode <52585150+citizenofcode@users.noreply.github.com>
2019-07-05 20:55:47 -0700

    Create eval.js

---
+++ Commands/eval.js
@@ -0,0 +1,68 @@
+const Command = require('../Struct/Command.js');
+const Discord = require('discord.js')
+const hastebin = require('hastebin-gen')
+
+class EvalCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: "eval",
+            description: "Evalutes JavaScript code.",
+            usage: "tl.eval [--s|silent] <code>",
+            aliases: ["e"],
+            ownerOnly: true,
+            example: ["tl.eval --s message.channels.send(\"hello\")"],
+            clientPermissions: ["EMBED_LINKS"]
+        })
+    }
+
+    async execute(message, client, args) {
+        if (!args.length) {
+            return message.channel.send("There's nothing to evalulate!");
+        } else {
+            function clean(text) {
+                if (typeof(text) === "string")
+                    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+                else
+                    return text;
+            }
+            try {
+                let code = args.join(" ");
+                let silent = false;
+                if (code.startsWith("--s") || code.startsWith("--silent")) {
+                    silent = true;
+                    code = args.slice(1).join(" ")
+                }
+                let evaled = eval(code);
+    
+                if (typeof evaled !== "string")
+                    evaled = require("util").inspect(evaled);
+                let send = `\`\`\`js\n${clean(evaled)}\n\`\`\``;
+                send = send.replace(client.config.token, "BOT_TOKEN");
+                let printOutput = send;
+                if (send.toString().length > 1024) {
+                    await hastebin(clean(evaled), {extension: "js"})
+                    .then(link => printOutput = `Output was too long - [Hastebin Link](${link})`)
+                    .catch(() => printOutput = `Output was too long - error generating hastebin link.`);
+                }
+                const embed = new Discord.RichEmbed()
+                    .setTitle("Evalulate Code")
+                    .setColor('GREEN')
+                    .addField("Input", `\`\`\`js\n${code}\n\`\`\``)
+                    .addField("Output", printOutput);
+                    if (!silent) {
+                message.channel.send(embed);
+                    }
+                
+            } catch (err) {
+                const error = `\`\`\`js\n${clean(err)}\n\`\`\``;
+                const errorembed = new Discord.RichEmbed()
+                    .setTitle("Error")
+                    .setColor('RED')
+                    .setDescription(error);
+                message.channel.send(errorembed);
+            }
+        }
+    }
+}
+
+module.exports = EvalCommand;
