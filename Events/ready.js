2e167cc4f3df77f50640377f4269c17ee4a2ba1f
citizenofcode <52585150+citizenofcode@users.noreply.github.com>
2019-07-05 21:09:07 -0700

    Prepare for more customizability

--- Events/ready.js
+++ Events/ready.js
@@ -6,6 +6,6 @@ module.exports = async client => {
     let hours = dt.getHours() > 12 ? dt.getHours() - 12 : dt.getHours();
     let time = dt.getHours() > 12 ? "PM" : "AM";
     const full = hours + ":" + (dt.getMinutes() >= 10 ? dt.getMinutes() : `0${dt.getMinutes()}`) + ":" + (dt.getSeconds() >= 10 ? dt.getSeconds() : `0${dt.getSeconds()}`) + " " + time;
-    client.channels.get("596769965760053272").send(new Discord.RichEmbed().setColor("GREEN").setTitle(`[${full}] - Bot is ready.`).setTimestamp())
+    client.channels.get(client.config.logChannel).send(new Discord.RichEmbed().setColor("GREEN").setTitle(`[${full}] - Bot is ready.`).setTimestamp())
     client.user.setActivity(`with ${client.users.size} members | tl.help`)
 }
