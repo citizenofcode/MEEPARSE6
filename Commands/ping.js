6838bdde97a30ec22eaea02e3228f81bbad6b878
Joe <52585150+citizenofcode@users.noreply.github.com>
2019-08-04 11:23:07 -0700

    Update ping.js

--- Commands/ping.js
+++ Commands/ping.js
@@ -1,26 +1,26 @@
 const Command = require('../Struct/Command.js');
-const Discord = require('discord.js')
+const Discord = require('discord.js');
 
 class PingCommand extends Command {
-    constructor(client) {
-        super(client, {
-            name: "ping",
-            description: "Latency and API response times.",
-            usage: "tl.ping",
-            example: ["tl.ping"],
-            aliases: ["pong"],
-            clientPermissions: ["EMBED_LINKS"]
-        })
-    }
+	constructor(client) {
+		super(client, {
+			name: 'ping',
+			description: 'Latency and API response times.',
+			usage: 'ping',
+			example: ['ping'],
+			aliases: ['pong'],
+			clientPermissions: ['EMBED_LINKS'],
+		});
+	}
 
-    execute(message, client, args) {
-        message.channel.send("Ping?")
-        .then(msg => msg.edit(new Discord.RichEmbed()
-        .setTitle("Pong!")
-        .setDescription(`**-** Latency: ${msg.createdTimestamp - message.createdTimestamp}ms\n**-** API Latency: ${Math.round(client.ping)}ms`)
-        .setColor(client.config.color)))
-        .catch(e => console.error(e))
-    }
+	exec(message, client) {
+		message.channel.send('Ping?')
+			.then(msg => msg.edit(new Discord.RichEmbed()
+				.setTitle('Pong!')
+				.setDescription(`**-** Latency: ${msg.createdTimestamp - message.createdTimestamp}ms\n**-** API Latency: ${Math.round(client.ping)}ms`)
+				.setColor(client.config.color)))
+			.catch(e => console.error(e));
+	}
 }
 
 module.exports = PingCommand;
