811b196097b2511216c4a07f66c135f14b045d67
Joe <52585150+citizenofcode@users.noreply.github.com>
2019-08-04 11:24:54 -0700

    Update Command.js

--- Struct/Command.js
+++ Struct/Command.js
@@ -1,18 +1,18 @@
 class Command {
-    constructor(client, {
-        name = null,
-        description = "No description.",
-        example = new Array(),
-        ownerOnly = false,
-        usage = "No usage provided.",
-        enabled = true,
-        location = null,
-        aliases = new Array(),
-        clientPermissions = new Array(),
-    }) {
-        this.client = client;
-        this.conf = { ownerOnly, enabled, aliases, location, clientPermissions};
-        this.help = { name, description, usage, example};
-    }
+	constructor(client, {
+		name = null,
+		description = 'No description.',
+		example = new Array(),
+		ownerOnly = false,
+		usage = 'No usage provided.',
+		enabled = true,
+		location = null,
+		aliases = new Array(),
+		clientPermissions = new Array(),
+	}) {
+		this.client = client;
+		this.conf = { ownerOnly, enabled, aliases, location, clientPermissions };
+		this.help = { name, description, usage, example };
+	}
 }
-module.exports = Command
+module.exports = Command;
