10e70608d1a3a9899e8e496193a8720e1403b04f
Joe <52585150+citizenofcode@users.noreply.github.com>
2019-08-04 11:25:47 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const OutpostClient = require('./Struct/OutpostClient.js');
-const client = new OutpostClient()
+const Client = require('./Struct/Client.js');
+const client = new Client();
 
-client.connect()
+client.connect();
