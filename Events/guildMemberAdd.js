e65927a72db121b8d454c9b7aa5cc541b1d18a9c
Joe <52585150+citizenofcode@users.noreply.github.com>
2019-08-04 11:24:10 -0700

    Update guildMemberAdd.js

--- Events/guildMemberAdd.js
+++ Events/guildMemberAdd.js
@@ -1,3 +1,3 @@
 module.exports = async (client) => {
-    client.user.setActivity(`with ${client.users.size} members | tl.help`)
-}
+	client.user.setActivity(`with ${client.users.size} members | ${client.config.prefix}help`);
+};
